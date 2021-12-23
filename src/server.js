d3e08fd20d0d7fe79fa113601492f54491eba723
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-12-23 14:54:31 +0530

    procfile added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./index")
 const connect = require("./configs/db.js")
 
-app.listen(7765, async function () {
+app.listen(process.env.PORT || 7765, async function () {
     await connect();
     console.log("listening on port 7765");
 })
